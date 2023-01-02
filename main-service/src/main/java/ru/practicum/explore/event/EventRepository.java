1d459f0cc2a58a7364c33bb1e4b5ccaf1afae9de
Sergey <sleontiev777@gmail.com>
2023-01-03 00:52:22 +0300

    finish

--- main-service/src/main/java/ru/practicum/explore/event/EventRepository.java
+++ main-service/src/main/java/ru/practicum/explore/event/EventRepository.java
@@ -23,15 +23,15 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
 
     List<Event> findAllByIdIn(Iterable<Integer> eventsId);
 
-    List<Event> findAllByInitiatorIdInAndStateInAndCategoryIdInAndEventDateIsAfterAndEventDateIsBefore
-            (Iterable<Integer> ids, List<State> states, Iterable<Integer> categories, LocalDateTime rangeStart,
-             LocalDateTime rangeEnd, PageRequest pageRequest);
+    List<Event> findAllByInitiatorIdInAndStateInAndCategoryIdInAndEventDateIsAfterAndEventDateIsBefore(
+            Iterable<Integer> ids, List<State> states, Iterable<Integer> categories, LocalDateTime rangeStart,
+            LocalDateTime rangeEnd, PageRequest pageRequest);
 
     @Query("select e from Event e Where (((?1 is null) or (lower(e.annotation) like CONCAT('%',?1,'%')))" +
             "or (((?1 is null) or lower( e.description) like CONCAT('%',?1,'%'))))" +
             "and (?6=false or (e.category.id in ?2))" +
             "and (?3 is null or e.paid=?3)and (e.eventDate between ?4 and ?5)" +
-            "and (e.participantLimit=0 or e.participantLimit>e.confirmedRequests) " +
+            "and (e.participantLimit=0 or e.participantLimit>e.confirmedRequests)" +
             "and e.state='PUBLISHED'")
     List<Event> findPublicEvents(String text, List<Integer> categoriesIds, Boolean paid, LocalDateTime start,
                                  LocalDateTime end, Boolean categories, Sort sort, PageRequest pageRequest);
